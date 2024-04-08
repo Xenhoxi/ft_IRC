ca31f92225f2db26681701d324cb61557bcb4e8a
leon <leonjerineck@gmail.com>
2024-04-08 12:33:14 +0200

    start channel class

---
+++ inc/channel.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   channel.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 12:32:05 by ljerinec          #+#    #+#             */
+/*   Updated: 2024/04/08 12:32:39 by ljerinec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+#include <iostream>
+#include <poll.h>
+
+class Channel
+{
+public:
+	
+
+private:
+	
+};
+
+
+#endif
