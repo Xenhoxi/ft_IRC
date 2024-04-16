d113376a60f27afd59a6d7a687c48d7af6791b88
leon <leonjerineck@gmail.com>
2024-04-16 10:41:10 +0200

    orga .hpp 2

--- inc/user.hpp
+++ inc/user.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   user.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smunio <smunio@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:10:01 by smunio            #+#    #+#             */
-/*   Updated: 2024/04/09 00:21:06 by smunio           ###   ########.fr       */
+/*   Updated: 2024/04/16 10:41:04 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,23 @@ class User
 public:
 	User();
 	~User();
+	int				get_status() const;
+	std::string 	get_nick();
+	struct pollfd   *get_fds() const;
+	void			add_data(std::string new_data);
+	
+	void			parsing(Server &server);
+	void			parse_negotiation(std::string line, Server &server);
 	void			negotiation(Server &server);
 	void			registration(Server &server);
-	int				get_status() const;
-	void			parse_negotiation(std::string line, Server &server);
-	void			parsing(Server &server);
+	void			send_message(std::string msg);
 	void			change_status(int status);
 	void            set_fds(int server_socket);
 	void			parse_command(std::string line, Server &server);
-	void			send_message(std::string msg);
-	std::string 	get_nick();
-	struct pollfd   *get_fds() const;
 	void			parse_op_cmd(const char *cmd, Server &server, std::string ch_name) const;
-	std::string		get_nick() const;
-	std::string		_data;
 
 private:
+	std::string		_data;
 	std::string     _nickname;
 	std::string		_username;
 	std::string		_password;
